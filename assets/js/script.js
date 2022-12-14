d866f48eb757f7ca2145b479def621987c13115c
Ryan Young <ryanbyoung1@gmail.com>
2022-12-14 11:31:25 -0500

    deleted extra fillCard function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,26 @@ var gifBtns = document.querySelectorAll('.gifBtn');
 var container = document.querySelector('.container');
 var movieContainer = document.querySelector(".movieContainer");
 var cardInfo = document.getElementById('drinks');
+var documentaryApiUrl = 'https://api.giphy.com/v1/gifs/search?q=research&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20'
+var horrorApiUrl = 'https://api.giphy.com/v1/gifs/search?q=terrifying&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
+var crimeApiUrl = 'https://api.giphy.com/v1/gifs/search?q=detective&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
+var comedyApiUrl = 'https://api.giphy.com/v1/gifs/search?q=laugh&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
+var dramaApiUrl = 'https://api.giphy.com/v1/gifs/search?q=dramatic&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
+var romanceApiUrl = 'https://api.giphy.com/v1/gifs/search?q=young_love&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
+var actionApiUrl = 'https://api.giphy.com/v1/gifs/search?q=explosion&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
+var westernApiUrl = 'https://api.giphy.com/v1/gifs/search?q=western&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
+var thrillerApiUrl = 'https://api.giphy.com/v1/gifs/search?q=anxious&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
+var familyApiUrl = 'https://api.giphy.com/v1/gifs/search?q=family&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
 var documentaries = ["The Rescue", "March of the Penguins", "Our Father", "Free Solo", "Fire of Love", "The Band", "Won't You Be My Neighbor", "Woodstock ...the movie", "Icarus", "Puping Iron", "The Cove", "Pele", "Apollo 11", "The Central Park Five", "Leaving Neverland", "Fyre", "Quincy", "The Thin Blue Line", "Zeitgeist The Movie", "Man On Wire", "The Salt of the Earth"];
-
 var horrorMovies = ["Smile", "Us", "Hereditary", "A Quiet Place", "It", "Get Out", "Scream", "Monster House", "Cabin in the Woods", "Let the Right One In", "Hellraiser", "The Invitation", "The Blair Witch Project", "Backcountry", "Krampus", "It Comes At Night", "The Mist", "Old", "The Ring", "The Thing"];
-
 var trueCrimeMovies = ["Zodiac", "The Hillside Strangler", "The Alphabet Killer", "Henry: Portrait of a Serial Killer", "The Secret Life: Jeffrey Dahmer", "To Catch a Killer", "The Deliberate Stranger", "The Hunt for the BTK Killer", "Monster", "The Riverman", "Dear Mr. Gacy", "8213: Gacy House", "10 Rillington Place", "The Capture of the Green River Killer", "The Atlanta Child Murders", "Citizen X", "The Boston Strangler", "Badlands", "In Cold Blood", "Helter Skelter", "Polytechnique"];
-
 var comedies = ["The General", "Duck Soup", "It Happened One Night", "Wedding Crashers", "Anchorman:The Legend of Ron Burgundy", "Walk Hard: The Dewey Cox Story", "The Hangover", "Office Space", "Dumb and Dumber", "Austin Powers: International Man of Mystery", "Old School", "Bedazzled", "Analyze This", "Midnight Run", "The Jerk", "Liar Liar", "Forgetting Sarah Marshall", "The 40-Year-Old Virgin", "Swingers", "Borat", "The Heat", "Superbad", "We're the Millers", "Bridesmaids"];
-
 var dramas = ["12 Years a Slave", "A Star Is Born", "A Streetcar Named Desire", "Almost Famous", "Animal Kingdom", "Atonement", "Beautiful Boy", "Before Sunrise", "Billy Elliot", "Boys Don\’t Cry", "Children of Men", "Daughters of the Dust", "Dunkirk", "Eternal Sunshine of the Spotless Mind", "Fences", "Gladiator", "Gone Girl", "Good Will Hunting", "Her", "Hidden Figures"];
-
 var romanceMovies = ["When Harry Met Sally", "Love Simon", "Always Be My Maybe", "Say Anything", "Straight Up", "Sleepless In Seattle", "How to Lose a Guy in 10 Days", "To All the Boys I\’ve Loved Before", "Sixteen Candles", "Silver Linings Playbook", "Groundhog Day", "Bridget Jone\’s Diary", "The Big Sick", "The Incredible", "Jessica James", "Love Actually", "Pretty Woman", "Mamma Mia", "13 Going on 30", "The Wedding Singer", "Notting Hill", "Just Go With It", "The Proposal", "27 Dresses", "10 Things I Hate About You", "My Big Fat Greek Wedding"];
-
 var actionMovies = ["The Bourne Ultimatum", "Mission Impossible - Rogue Nation", "John Wick", "Enter the Dragon", "House of Flying Daggers", "Mad Max: Fury Road", "The Terminator", "Men in Black", "Die Hard", "Casino Royale", "Inside Man", "The Dark Knight", "Raiders of the Lost Ark", "Avengers: Endgame", "The Lord of the Rings: The Two Towers", "Everything Everywhere All at Once", "Aliens", "Police Story", "Kill Bill", "The Matrix", "Lethal Weapon", "Rambo", "Point Break", "Skyfall", "Inception"];
-
 var feelGoodMovies = ["The Parent Trap", "Spirited Away", "Ratatouille", "Hunt for the Wilderpeople", "Captain Fantastic", "Stand By Me", "Grand Budapest Hotel", "Uptown Girls", "The Terminal", "Clueless", "The Princess Diaries", "Little Miss Sunshine", "The Breakfast Club", "Fantastic Mr. Fox", "Amélie", "Dennis The Menace", "Little Rascals", "Groundhog Day", "The Incredibles", "Jumanji", "The Wizard of Oz", "The Sound of Music"];
-
 var thrillers = ["Gone Girl", "The Invisible Man", "Woman in the Window", "The Butterfly Effect", "Enough", "The Stranger", "Black Swan", "Shutter Island, Parasite", "The Girl on the Train", "Fractured", "Prisoners", "Inception", "Zodiac", "The Invitation", "Nocturnal Animals", "The Gift", "Nightcrawler", "The Machinist", "Mulholland Drive", "The Shining", "Cape Fear"];
-
 var westerns = ["The Power of the Dog", "Hell or High Water", "Tombstone", "The Hateful Eight", "The Salvation", "Blackthorn", "The Magnificent Seven", "Dead Man", "The Horse Whisperer", "The Keeping Room", "In a Valley of Violence", "Silverado", "The Ballad of Lefty Brown", "Red Hill", "Hostiles", "Appaloosa", "Open Range", "El Topo", "The Long Riders", "Hud", "The Homesman", "How the West Was Won"];
-
 var smart = ["Dark & Stormy", "Negroni", "Old Fashioned", "French 75", "Mint Julep"];
 var scared = ["Bloody Mary", "BoilerMaker", "Corpse Reviver", "Cuba Libra", "Grasshopper"];
 var investigative = ["Sidecar", "Blue Lagoon", "Bellini", "Black & Tan", "Gin Fizz"];
@@ -34,34 +34,15 @@ var thriller = ["Moscow Mule", "Sazerac", "Boulevardier", "Gimlet", "Mind Eraser
 var yeehaw = ["Whiskey Sour", "Polama", "Cement Mixer", "Long Island Iced Tea", "Lynchburg Lemonade", "Mudslide"];
 
 var i = 4
-// var search = investigative[1]  
-// ---this is a tester for broken searches
-// scared, investigative all good
-
 
 var drinkFetch = function () {
   for (var j = 0; j < 3; j++) {
 
     var drinks = [smart, scared, investigative, laugh, dramatic, youngLove, explosion, family, thriller, yeehaw,];
 
-
     var search = drinks[i][Math.floor(Math.random() * drinks[i].length)];
 
     requestURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + search;
-
-    var fillCards = function (name, picture, mainIngred, instructions) {
-
-      console.log(name);
-      console.log(picture);
-      console.log(mainIngred);
-      console.log(instructions);
-
-      var drinks = [explosion, smart, scared, investigative, laugh, dramatic, youngLove, yeehaw, thriller, family];
-
-
-      var search = drinks[j][Math.floor(Math.random() * drinks[j].length)];
-
-      requestURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + search;
 
       var fillCards = function (name, picture, mainIngred, instructions) {
 
@@ -127,7 +108,6 @@ var drinkFetch = function () {
         });
 
     }
-  }
 };
 drinkFetch();
 
@@ -135,16 +115,6 @@ drinkFetch();
 // data.images.looping.mp4
 // var apiUrl = 'api.giphy.com/v1/gifs/search?q=' + mood + '&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20'
 
-var documentaryApiUrl = 'https://api.giphy.com/v1/gifs/search?q=research&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20'
-var horrorApiUrl = 'https://api.giphy.com/v1/gifs/search?q=terrifying&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
-var crimeApiUrl = 'https://api.giphy.com/v1/gifs/search?q=detective&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
-var comedyApiUrl = 'https://api.giphy.com/v1/gifs/search?q=laugh&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
-var dramaApiUrl = 'https://api.giphy.com/v1/gifs/search?q=dramatic&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
-var romanceApiUrl = 'https://api.giphy.com/v1/gifs/search?q=young_love&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
-var actionApiUrl = 'https://api.giphy.com/v1/gifs/search?q=explosion&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
-var westernApiUrl = 'https://api.giphy.com/v1/gifs/search?q=western&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
-var thrillerApiUrl = 'https://api.giphy.com/v1/gifs/search?q=anxious&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
-var familyApiUrl = 'https://api.giphy.com/v1/gifs/search?q=family&api_key=FsySzgOBYv5zqlKccJT94Jx4XGUmM90i&rating=pg&limit=20';
 
 var requestUrls = [documentaryApiUrl, horrorApiUrl, crimeApiUrl, comedyApiUrl, dramaApiUrl, romanceApiUrl, actionApiUrl, westernApiUrl, thrillerApiUrl, familyApiUrl];
 container.addEventListener('click', function (event) {
